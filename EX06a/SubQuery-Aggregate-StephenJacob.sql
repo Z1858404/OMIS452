0a3b81cebc33ddc7ca6a33774f91fb10a862832d
Stephen Chartrand <55199572+Z1858404@users.noreply.github.com>
2019-10-28 16:41:25 -0500

    Update SubQuery-Aggregate-StephenJacob.sql

--- EX06a/SubQuery-Aggregate-StephenJacob.sql
+++ EX06a/SubQuery-Aggregate-StephenJacob.sql
@@ -1,9 +1,9 @@
 /* Query- by Stephen C and Jacob H - Aggregate Function */
 
-SELECT EmployeeNumber, HoursWorked
-FROM assignment AS a
+SELECT e.EmployeeNumber, e.FirstName, e.LastName, a.HoursWorked
+FROM assignment AS a, employee AS e
 WHERE HoursWorked >
   (SELECT AVG(HoursWorked)
    FROM assignment
-   WHERE EmployeeNumber=a.EmployeeNumber)
-GROUP BY EmployeeNumber;
+   WHERE e.EmployeeNumber=a.EmployeeNumber)
+ORDER BY e.EmployeeNumber;
