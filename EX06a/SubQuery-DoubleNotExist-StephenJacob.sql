2140b5747988f7c66170fe263f7b04e2dceb791c
Stephen Chartrand <55199572+Z1858404@users.noreply.github.com>
2019-10-28 17:41:31 -0500

    Update SubQuery-DoubleNotExist-StephenJacob.sql

--- EX06a/SubQuery-DoubleNotExist-StephenJacob.sql
+++ EX06a/SubQuery-DoubleNotExist-StephenJacob.sql
@@ -1,13 +1,15 @@
 /* Query - by Stephen C and Jacob H - Double Not Exists */
 
-SELECT DISTINCT EmployeeNumber
-FROM employee
+SELECT DISTINCT e.EmployeeNumber
+FROM employee AS e
 WHERE NOT EXISTS(
+  SELECT *
+  FROM project as p
+  WHERE NOT EXISTS(
     SELECT *
-    FROM project
-    WHERE NOT EXISTS(
-        SELECT *
-        FROM assignment
-        WHERE assignment.EmployeeNumber=employee.EmployeeNumber
-    )
-);
+    FROM assignment AS a
+    WHERE (a.EmployeeNumber=e.EmployeeNumber) AND (a.ProjectID=p.ProjectID)
+  )
+)
+GROUP BY EmployeeNumber
+;
